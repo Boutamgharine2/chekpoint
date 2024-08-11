eba87ea5150aef40fd7956c3c23fcb0022d93257
aboutamgh <anouarboutamgharin@gmail.com>
2024-08-11 14:55:13 +0100

    kjddsj

---
+++ go.mod
@@ -0,0 +1,3 @@
+module jhb
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(RepeatAlpha("abc"))
+	fmt.Println(RepeatAlpha("Choumi."))
+	fmt.Println(RepeatAlpha(""))
+	fmt.Println(RepeatAlpha("abacadaba 01!"))
+}
+
+func RepeatAlpha(s string) string {
+	str := ""
+	for i := 0; i < len(s); i++ {
+		str += canada(rune(s[i]))
+	}
+	return str
+}
+
+func canada(r rune) string {
+	s := ""
+	var v rune
+	if r >= 'a' && r <= 'z' {
+		v = r - 96
+		for i := 0; i < int(v); i++ {
+			s += string(r)
+		}
+	} else if r >= 'A' && r <= 'Z' {
+		v = r - 64
+		for i := 0; i < int(v); i++ {
+			s += string(r)
+		}
+	} else {
+		s += string(r)
+	}
+	return s
+}
