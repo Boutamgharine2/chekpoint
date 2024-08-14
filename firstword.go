d65ecbd3db7284efacb4665229b0a539864858cf
aboutamgh <anouarboutamgharin@gmail.com>
2024-08-14 15:23:23 +0100

    hgdzhh

---
+++ firstword.go
@@ -0,0 +1,31 @@
+package main
+import "fmt"
+
+
+func main() {
+    fmt.Print(FirstWord("hello there"))
+    fmt.Print(FirstWord(""))
+    fmt.Print(FirstWord("hello   .........  bye")) 
+	fmt.Print(FirstWord("anouar boutamgharine est ")) 
+	fmt.Print(FirstWord("  jdsjkd kjkjdj")) 
+
+
+}
+
+
+
+func FirstWord(s string) string {
+	c:=""
+	if s=="" {
+		return ""+"\n"
+	}
+    for i,v := range s {
+		if v == ' ' && i!=0 && c!=""{
+			return c+"\n"
+		} else if v==' '  && c==""{
+			continue
+		}
+		c+=string(v)
+	}
+	return c
+}
