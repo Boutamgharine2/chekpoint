0fc7becc1a844dd5d165c2240a690490376e51e0
Boutamgharine2 <anouarboutamgh@gmail.com>
2024-08-27 16:43:47 +0100

    yesss

---
+++ findprevprime.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func FindPrevPrime(nb int) int {
+	for !Isprime(nb) {
+		nb -= 1
+	}
+	return nb
+}
+
+func Isprime(n int) bool {
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(FindPrevPrime(100))
+	fmt.Println(FindPrevPrime(15))
+}
