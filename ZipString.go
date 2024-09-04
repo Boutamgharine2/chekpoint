1c35c1f9c23e795352ec3f044e7211ea9736dad1
Boutamgharine2 <anouarboutamgharin@gmail.com>
2024-09-04 15:08:25 +0100

    jhjh

--- ZipString.go
+++ ZipString.go
@@ -8,8 +8,7 @@ func ZipString(s string) string {
 	for i := 0; i < len(s); i++ {
 		if i!=0 && s[i]==s[i-1] {
 			continue
-		}
-		if len(s) > i+c {
+		}else if len(s) > i+c {
 			for s[i] == s[i+c] {
 				c++
 			}
@@ -41,7 +40,7 @@ func ZipString(s string) string {
 
 func Contien(b byte, s string) bool {
 	for i := 0; i < len(s); i++ {
-		if s[i] == b {1T1h2e1 1q2u1i1c1k1 1b1r1o2w1n1 1f1o1x1 1j2u1m1p1s1 1o1v1e1r1 1t1h1e1 1l3a1z1y1 1d1o1g
+		if s[i] == b {
 
 			return true
 		}
